8b710ea3d653f2c97985128fc6f9de2007ae5681
Elise Hove <elisehove@Elises-MacBook-Pro.local>
2018-01-07 13:32:22 +0100

    .

---
+++ javascript.js
@@ -0,0 +1,29 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function fold() {
+    var x = document.getElementById("timeline");
+    if (x.className === "topnav") {
+        x.className += "responsive";
+    } else {
+        x.className = "topnav";
+    }
+
+}
+
+
+
+function visCV() {
+    var x = document.getElementById("timeline");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
